2fc4c479a30f29dcb666c4865540b3e19028cd34
Yiting Pei <69138287+A-Beta-in-Iceland@users.noreply.github.com>
2023-05-07 16:13:44 -0400

    Update HadmardProduct.m

--- Spin_Boson/HadmardProduct.m
+++ Spin_Boson/HadmardProduct.m
@@ -4,7 +4,7 @@ function out = HadmardProduct(A, B)
    [coff_B_I, coff_B_x, coff_B_y, coff_B_z] = seperate(B)
    coff0 = coff_A_I * coff_B_I + coff_A_z * coff_B_z
    coff1 = -2 * coff_A_y * coff_B_y
-   coff2 = i * (coff_A_x * coff_B_y + coff_B_x * coff_A_y)
+   coff2 = coff_A_x * coff_B_y + coff_B_x * coff_A_y
    coff3 = coff_A_I * coff_B_z + coff_A_z * coff_B_I
    out = coff0 * I + coff1 * sigma_x + coff2 * sigma_y + coff3 * sigma_z
 end
