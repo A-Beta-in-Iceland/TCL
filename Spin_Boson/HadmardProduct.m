530c6257b33b10b901f94052877491500dadd647
Yiting Pei <69138287+A-Beta-in-Iceland@users.noreply.github.com>
2023-06-15 16:17:34 +0800

    Update HadmardProduct.m
    some minor updates with notation

--- Spin_Boson/HadmardProduct.m
+++ Spin_Boson/HadmardProduct.m
@@ -1,10 +1,10 @@
 function out = HadmardProduct(A, B)
-   syms I sigma_x sigma_y sigma_z 
-   [coff_A_I, coff_A_x, coff_A_y, coff_A_z] = seperate(A)
-   [coff_B_I, coff_B_x, coff_B_y, coff_B_z] = seperate(B)
-   coff0 = coff_A_I * coff_B_I + coff_A_z * coff_B_z
-   coff1 = coff_A_x * coff_B_x - coff_A_y * coff_B_y
-   coff2 = coff_A_x * coff_B_y + coff_B_x * coff_A_y
-   coff3 = coff_A_I * coff_B_z + coff_A_z * coff_B_I
-   out = coff0 * I + coff1 * sigma_x + coff2 * sigma_y + coff3 * sigma_z
+   syms I x y z
+   [coff_A_I, coff_A_x, coff_A_y, coff_A_z] = seperate(A);
+   [coff_B_I, coff_B_x, coff_B_y, coff_B_z] = seperate(B);
+   coff0 = coff_A_I * coff_B_I + coff_A_z * coff_B_z;
+   coff1 = coff_A_x * coff_B_x - coff_A_y * coff_B_y;
+   coff2 = coff_A_x * coff_B_y + coff_B_x * coff_A_y;
+   coff3 = coff_A_I * coff_B_z + coff_A_z * coff_B_I;
+   out = coff0 * I + coff1 * x + coff2 * y + coff3 * z;
 end
