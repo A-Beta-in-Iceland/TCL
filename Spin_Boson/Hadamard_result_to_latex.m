46dc755e4d31e8b1484b2a482dfa68fa55ca7db7
A-Beta-in-Iceland <69138287+A-Beta-in-Iceland@users.noreply.github.com>
2023-06-23 17:53:38 +0800

    Plug actual system correlation operator in

--- Spin_Boson/Hadamard_result_to_latex.m
+++ Spin_Boson/Hadamard_result_to_latex.m
@@ -1,13 +1,21 @@
 % Input string
-line = '-[I*b*(conj(g11(b, a))/2 + conj(g22(b, a))/2 - conj(g11(b, t))/2 - conj(g22(b, t))/2) + b*x*(conj(g12(b, a))/2 + conj(g21(b, a))/2 - conj(g12(b, t))/2 - conj(g21(b, t))/2) - b*y*((conj(g12(b, a))*1i)/2 - (conj(g21(b, a))*1i)/2 - (conj(g12(b, t))*1i)/2 + (conj(g21(b, t))*1i)/2) + b*z*(conj(g11(b, a))/2 - conj(g22(b, a))/2 - conj(g11(b, t))/2 + conj(g22(b, t))/2),[[a, a; a, a],0]I*a*(conj(g11(a, 0))/2 + conj(g22(a, 0))/2 - conj(g11(a, b))/2 - conj(g22(a, b))/2) + a*x*(conj(g12(a, 0))/2 + conj(g21(a, 0))/2 - conj(g12(a, b))/2 - conj(g21(a, b))/2) - a*y*((conj(g12(a, 0))*1i)/2 - (conj(g21(a, 0))*1i)/2 - (conj(g12(a, b))*1i)/2 + (conj(g21(a, b))*1i)/2) + a*z*(conj(g11(a, 0))/2 - conj(g22(a, 0))/2 - conj(g11(a, b))/2 + conj(g22(a, b))/2)]';
-
+% line = '-[I*b*(conj(g11(b, a))/2 + conj(g22(b, a))/2 - conj(g11(b, t))/2 - conj(g22(b, t))/2) + b*x*(conj(g12(b, a))/2 + conj(g21(b, a))/2 - conj(g12(b, t))/2 - conj(g21(b, t))/2) - b*y*((conj(g12(b, a))*1i)/2 - (conj(g21(b, a))*1i)/2 - (conj(g12(b, t))*1i)/2 + (conj(g21(b, t))*1i)/2) + b*z*(conj(g11(b, a))/2 - conj(g22(b, a))/2 - conj(g11(b, t))/2 + conj(g22(b, t))/2),[[a, a; a, a],0]I*a*(conj(g11(a, 0))/2 + conj(g22(a, 0))/2 - conj(g11(a, b))/2 - conj(g22(a, b))/2) + a*x*(conj(g12(a, 0))/2 + conj(g21(a, 0))/2 - conj(g12(a, b))/2 - conj(g21(a, b))/2) - a*y*((conj(g12(a, 0))*1i)/2 - (conj(g21(a, 0))*1i)/2 - (conj(g12(a, b))*1i)/2 + (conj(g21(a, b))*1i)/2) + a*z*(conj(g11(a, 0))/2 - conj(g22(a, 0))/2 - conj(g11(a, b))/2 + conj(g22(a, b))/2)]';
+line = 'x*(cos(t*(del^2 + eps^2)^(1/2)) + (2*del^2*sin((t*(del^2 + eps^2)^(1/2))/2)^2)/(del^2 + eps^2)) - (eps*y*sin(t*(del^2 + eps^2)^(1/2)))/(del^2 + eps^2)^(1/2) + (2*del*eps*z*sin((t*(del^2 + eps^2)^(1/2))/2)^2)/(del^2 + eps^2)';
 % Replace conj(g\d\d(...)) with \Gamma^*_{$1$2}(...)
-pattern = 'conj\(g(\d)(\d)(.*?)\)';
-replacement = '\\Gamma^*_{$1$2}$3'
+% pattern = 'conj\(g(\d)(\d)(.*?)\)';
+% replacement = '\\Gamma^*_{$1$2}$3'
+% line_modified = regexprep(line, pattern, replacement);
+% 
+% pattern = '[+-]'
+% replacement = '\&$0'
+% line_modified = regexprep(line_modified, pattern, replacement);
+
+pattern = 'del';
+replacement = '\\Delta';
 line_modified = regexprep(line, pattern, replacement);
 
-pattern = '[+-]'
-replacement = '\&$0'
+pattern = 'eps';
+replacement = '\\epsilon';
 line_modified = regexprep(line_modified, pattern, replacement);
 
 % Display the modified string
